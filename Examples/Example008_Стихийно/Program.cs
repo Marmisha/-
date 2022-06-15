df172547aea0188206f7f2048c8559771a113a86
Marmisha <marmisha1991@rambler.ru>
2022-06-15 23:25:42 +0300

    Ex08 resave ver2

--- Examples/Example008_Стихийно/Program.cs
+++ Examples/Example008_Стихийно/Program.cs
@@ -1,22 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Процедурный"/классический вариант написания кода:
+int Max (int arg1, int arg2, int arg3) // будем сравнивать по 3 значения, потом еще 3 значения, потом "победителей"...
+//теперь пишем тело метода:
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result; 
+}
+// Таким образом, у нас теперь есть вспомогательный механизм, который ищет максимальное из трех чисел. Он заменяет наши 8 строчек кода с if из стихийного метода
 int a1 = 15;
 int b1 = 21;
 int c1 = 39;
-int a2 = 12;
-int b2 = 23;
+int a2 = 122;
+int b2 = 23132;
 int c2 = 33;
 int a3 = 13;
 int b3 = 23;
 int c3 = 33;
-
-int max = a1;
-if (b1 > max) max = b1;
-if (c1 > max) max = c1;
-if (a2 > max) max = a2;
-if (b2 > max) max = b2;
-if (c2 > max) max = c2;
-if (a3 > max) max = a3;
-if (b3 > max) max = b3;
-if (c3 > max) max = c3;
-
+//определим три "финалиста":
+int max1=Max(a1, b1, c1); // после того, как отработает этот кусочек кода, мы получим максимум из первой тройки "игроков"? можно вывести Console.WriteLine (max1);.
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+/* или короче и аккуратнее вместо 4 строчек - одну:
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+*/
 Console.WriteLine (max);
