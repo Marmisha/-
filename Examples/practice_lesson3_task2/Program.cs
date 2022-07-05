934b1a7195f9c113dc068d196b2cb636e6fd5fb2
Marmisha <marmisha1991@rambler.ru>
2022-07-05 23:12:25 +0300

    PracticeLesson added

---
+++ Examples/practice_lesson3_task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (x и y).
+// x от 0 до бесконечности попадает....
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+//int x = Prompt("Введите x => ");
+//int x = Prompt("Введите y => ");
+  
+int quater = Prompt("Введите номер четверти => ");
+
+if (quater == 1){
+    Console.Write("Значения x > 0, y > 0");
+}
+if (quater == 2){
+    Console.Write("Значения x < 0, y > 0");
+}
+if (quater == 3){
+    Console.Write("Значения x < 0, y < 0");
+}
+if (quater == 4){
+    Console.Write("Значения x > 0, y < 0");
+}
