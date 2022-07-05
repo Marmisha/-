03183b420d8f60503abd2884bad618d0b0f66633
Marmisha <marmisha1991@rambler.ru>
2022-07-05 23:12:46 +0300

    PracticeLesson added

---
+++ Examples/practice_lesson3_task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов числе от 1 до N.
+//5-> 1,4,9,16,25.
+//2->124
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);// Иными словами: что мы введем, вернется как int.
+}
+int N = Prompt("Введите число N: ");
+Console.WriteLine($"Вы ввели число {N}");
+Console.WriteLine($"Выясним, какие числа лежат от 1 до {N}.");
+
+int i = 0;
+
+while (i < N)
+{
+    i++;
+     Console.WriteLine(i * i);
+}
