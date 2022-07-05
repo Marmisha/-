bc6b6a2f0f4554997bf0c526e773bdc4d7818fc2
Marmisha <marmisha1991@rambler.ru>
2022-07-05 23:11:50 +0300

    PracticeLesson added

---
+++ Examples/practice_lesson2_task5.0/Program.cs
@@ -0,0 +1,22 @@
+﻿//Ввести трехзначное число и первратить его в массив
+Console.Clear();
+void printArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+
+}
+
+Console.WriteLine("Введите трехзначное число");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+
+int[] array = new int[3];
+
+array[0] = numberInt / 100;
+array[1] = numberInt % 100 / 10;
+array[2] = numberInt % 10;
+
+printArray(array); // видимо, для вывода массива нужна именно такая команда
