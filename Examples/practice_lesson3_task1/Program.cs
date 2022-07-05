2524e2b5ffafbf96b3581e472a8b62c5d9164885
Marmisha <marmisha1991@rambler.ru>
2022-07-05 23:12:16 +0300

    PracticeLesson added

---
+++ Examples/practice_lesson3_task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+//причем Ч не равно 0 или Y не равно 0 и выдает номер четверти плоскости, 
+//в которой находится эта точка.
+
+int Prompt(string message){
+    Console.WriteLine(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+int x = Prompt("Введите X =>");
+int y = Prompt("Введите Y =>");
+
+if (x == 0|| y==0){
+    Console.Write("Одна из координат равна 0");
+}
+
+if (x > 0 && y> 0)
+{
+    Console.WriteLine("1 четверть");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("2 четверть");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("3 четверть");
+}
+
+if (x > 0 && y <  0)
+{
+    Console.WriteLine("4 четверть");
+}
