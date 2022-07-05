bb99f8fd7ca3fa0edb6e690b95fca9e618bc2336
Marmisha <marmisha1991@rambler.ru>
2022-07-05 23:10:57 +0300

    PracticeLesson added

---
+++ Examples/practice_lesson2_task1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа.
+456->6
+782->2
+918->8
+*/
+Console.Clear();
+Console.WriteLine("Привет, введи трехзначное число!");
+string numString = Console.ReadLine();
+int numInt = int.Parse(numString);
+
+if (numInt > 99 && numInt < 1000) // проверка правильности введенного числа
+    {
+    Console.Write($"Итак, это {numInt}, а последняя цифра числа: " + numInt % 10);
+    }   
+    else
+    {
+        Console.WriteLine("Введено неверное число!");
+    }
