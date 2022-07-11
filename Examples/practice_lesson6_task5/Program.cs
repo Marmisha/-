ecdd21e175b1b47777ef530ad0d680250adb9da0
Marmisha <marmisha1991@rambler.ru>
2022-07-11 22:42:13 +0300

    Practice lesson added

---
+++ Examples/practice_lesson6_task5/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача: напишите задачу, которая будет создавать копию заданного массива с помощтю поэлементного копирования:
+
+Console.Clear();
+
+
+int[] CreateAndFillArray(int len, int min, int max)
+{Random rnd= new Random();
+        int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = rnd.Next(min, max+1);
+    }
+    return arr;
+}
+
+
+
+
+void printArray(int[] arr)
+{
+    foreach (int element in arr)
+    {
+        System.Console.Write($"{element};   ");
+    }
+    System.Console.WriteLine();
+}
+
+
+
+
+int[] CopyArray(int[] arr)
+{
+    int[] arr1 = new int[arr.Length];
+    for (int i=0; i<arr.Length; i++)
+    {
+        arr1[i] = arr[i];
+    }
+    return    arr1;
+     }
+
+
+int [] myArray =CreateAndFillArray(5,1,10);
+System.Console.Write("Первоначальный массив: ");
+printArray(myArray);
+int [] myCopiedArray = CopyArray(myArray);
+System.Console.Write("Копия массива: ");
+printArray(myCopiedArray);
+
+
+
+
+
