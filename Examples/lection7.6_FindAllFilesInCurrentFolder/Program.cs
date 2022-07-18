145e39c4715944bc21ce4ae37fb1bb0bf40ee669
Marmisha <marmisha1991@rambler.ru>
2022-07-18 15:55:09 +0300

    Practice lesson added

---
+++ Examples/lection7.6_FindAllFilesInCurrentFolder/Program.cs
@@ -0,0 +1,15 @@
+﻿// Поиск даты создания папки по его местонахождению:
+string path = "C:/Users/User/Documents/GeekBrains/C sharp/Examples/Examples";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo [] fi=di.GetFiles();
+
+
+//Поиск всех файлов в текущей папке:
+for(int i =0; i<fi.Length; i++) // найдем все файлы в папке
+{
+    System.Console.WriteLine(fi[i].Name);
+}
+
+
+//Теперь  напишем рекурсию, которая пробежится по папкам и посмотрит, что в них есть (следующий task)
