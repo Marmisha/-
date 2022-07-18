73a7c94da898555cb94ca6a524fc0d76f147449e
Marmisha <marmisha1991@rambler.ru>
2022-07-18 20:21:50 +0300

    Practice lesson added

---
+++ Examples/lection7.8_/Program.cs
@@ -0,0 +1,12 @@
+﻿//Пирамидки
+
+void Towers(string from = "1", string to = "3", string anotherOne = "2", int count = 3)
+// 1 - откуда берем блинчики; 3 - куда кладем блинчики; 
+//2 - просто еще один шпиль; count - кол-во блинов
+{
+    if (count > 1) Towers(from, anotherOne, to, count - 1);
+    Console.WriteLine($"{from} >> {to}");
+    if (count > 1) Towers(anotherOne, to, from, count - 1);
+}
+
+Towers();
