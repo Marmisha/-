e60119a9e5e8bc82236eb72a5a88b51365339d59
Marmisha <marmisha1991@rambler.ru>
2022-08-03 22:28:58 +0300

    ractice lesson added

---
+++ Examples/practice_lesson9_task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Семинар 9 Задача 1
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+// N=5 -> "1,2,3,4,5"
+// N=6 -> "1,2,3,4,5,6"
+
+
+Console.Clear();
+
+
+
+int GetIntFromReadLine(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+
+//Метод, выводящий на экран все натуральные числа от 0 до заданного числа N:
+void PrintNatural(int number)
+{
+    if (number <= 0) // выходим, когда меньше или иравно 0.Если больше 0, идем по действиям Ифа
+    {
+        return;
+    }
+    System.Console.Write($"{number} v "); 
+    PrintNatural(number - 1);
+    System.Console.Write($"{number} ^ ");
+
+        // в зависимости о ттго, где относительно рекурсии стоит cw, меняется результат вывода
+
+}
+
+int N = GetIntFromReadLine("Введите число N: ");
+System.Console.WriteLine("Наше число: " + N + ".");
+
+PrintNatural(N);
