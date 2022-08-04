0347fc33616adc742255e62c4e2bab189daefe8d
Marmisha <marmisha1991@rambler.ru>
2022-08-04 21:50:31 +0300

    ractice lesson added

---
+++ Examples/practice_lesson9_task4/Program.cs
@@ -0,0 +1,32 @@
+﻿//Семинар 9 задача 4
+// Напишите программу, которая на вход принимает два числа A и B 
+// и возводит A в целую степень B с помощью рекурсии.
+
+Console.Clear();
+
+
+
+int GetIntFromReadLine(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+
+// Метод возведения числа в степень с помощью рекурсии
+int NumberPow(int inputNumberOne, int inputNumberTwo)
+{
+
+    if (inputNumberTwo == 0) // Для этой задачи пусть будет так
+    {
+        return 1;
+    }
+    return (NumberPow(inputNumberOne, inputNumberTwo - 1) * inputNumberOne);
+}
+
+
+
+int A = GetIntFromReadLine("Введите число A: ");
+int B = GetIntFromReadLine("Введите число B: ");
+int myResult = NumberPow(A, B);
+System.Console.WriteLine($"{A} в степени {B} равно {myResult}.");
